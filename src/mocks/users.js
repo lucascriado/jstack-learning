1ea8c9f76c81267b666656af2422b8a586c1257e
lucascriado <lucascriado33@gmail.com>
2024-12-28 12:56:34 -0300

    feat: leearning req and res

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [ 
+    {
+        id: 1,
+        name: 'Mateus'
+    },
+    {
+        id: 2,
+        name: 'Bruno'
+    },
+    {
+        id: 3,
+        name: 'José'
+    },
+    {
+        id: 4,
+        name: 'Lucas'
+    }
+]
